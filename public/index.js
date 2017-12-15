4ab1c9c91c63645af30888007399498f6aada309
Peter Jang <peter.x.jang@gmail.com>
2017-12-14 21:26:05 -0600

    Install Vue.js frontend

---
+++ public/index.js
@@ -0,0 +1,22 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  mounted: function() {},
+  methods: {},
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }]
+});
+
+var app = new Vue({
+  el: "#app",
+  router: router
+});
