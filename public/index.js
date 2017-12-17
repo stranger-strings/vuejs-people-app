7a1d23e8805fc102fc950abc232f7e10e802887c
Peter Jang <peter.x.jang@gmail.com>
2017-12-17 09:17:44 -0600

    Add fake people and display with v-for

--- public/index.js
+++ public/index.js
@@ -4,7 +4,33 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      people: [
+        {
+          name: "Bob",
+          bio:
+            "Small batch salvia Marfa chillwave delectus, odio forage art party laborum street art minim fixie locavore hoodie mollit.",
+          bioVisible: true
+        },
+        {
+          name: "Alice",
+          bio:
+            "Tattooed letterpress gluten-free ugh, adipisicing scenester church-key gentrify tousled gastropub pour-over Shoreditch asymmetrical lomo High Life.",
+          bioVisible: true
+        },
+        {
+          name: "Tibor",
+          bio:
+            "Incididunt photo booth ethical reprehenderit adipisicing. Echo Park readymade Bushwick distillery Tonx. +1 semiotics qui duis literally.",
+          bioVisible: true
+        },
+        {
+          name: "Živa",
+          bio:
+            "Excepteur shabby chic semiotics Marfa, quinoa try-hard polaroid pariatur banh mi selfies incididunt brunch trust fund. Ethical dolor PBR&B Tumblr.",
+          bioVisible: true
+        }
+      ]
     };
   },
   mounted: function() {},
