646fd4cbc3921c04513fa9d4e7e9047d7392b77e
Peter Jang <peter.x.jang@gmail.com>
2017-12-28 18:35:43 -0600

    Add sample google map

--- public/index.js
+++ public/index.js
@@ -16,6 +16,11 @@ var HomePage = {
         this.people = response.data;
       }.bind(this)
     );
+    console.log(google);
+    var map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 41.8781, lng: -87.6298 },
+      zoom: 12
+    });
   },
   methods: {
     uploadFile: function(event) {
